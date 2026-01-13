a321db07fae372d247ad478b889bf3d7b5d61fe3
amoghsharma24 <amoghsharma02@gmail.com>
2026-01-13 17:21:52 +1100

    fixed importss

--- We are not in orbit yet/backend/src/main/java/com/orbital/backend/repository/SatelliteRepository.java
+++ We are not in orbit yet/backend/src/main/java/com/orbital/backend/repository/SatelliteRepository.java
@@ -1,10 +1,12 @@
 package com.orbital.backend.repository;
 
-import com.orbital.backend.model.Satellite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.orbital.backend.model.Satellite;
 
 @Repository
 public interface SatelliteRepository extends JpaRepository<Satellite, Long> {
     boolean existsByName(String name);
+    Satellite findByName(String name);
 }
